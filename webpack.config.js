b6e12b52e69378ad1791d7daa71e5020c8c64c55
Vladimir Molodozhan <vladimir.molodozhan@toptal.com>
2023-06-26 02:49:15 +0300

    Added version plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,25 @@
 const path = require('path');
+const fs = require('fs')
+const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const gitRevisionPlugin = (() => {
+  // This horrible hack is necessary because the GitRevisionPlugin
+  // always adds "git" as a prefix to the given command :(
+  // ":" is "do nothing" in bash
+  const runCommand = (command = ':') => `log -1 2> /dev/null || ${command}`
+
+  return fs.existsSync('.git')
+    ? // allow non-annotated tags, which we use
+      new GitRevisionPlugin({ versionCommand: 'describe --always --tags' })
+    : new GitRevisionPlugin({
+        commithashCommand: runCommand(`echo '${process.env.COMMITHASH}'`),
+        versionCommand: runCommand(`echo '${process.env.VERSION}'`),
+        lastCommitDateTimeCommand: runCommand(),
+      })
+})()
 
 module.exports = {
   mode: 'development',
@@ -31,5 +50,12 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ template: './index.template.html' }),
+    new GitRevisionPlugin(),
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(gitRevisionPlugin.version()),
+      COMMITHASH: JSON.stringify(gitRevisionPlugin.commithash()),
+      BRANCH: JSON.stringify(gitRevisionPlugin.branch()),
+      LASTCOMMITDATETIME: JSON.stringify(gitRevisionPlugin.lastcommitdatetime()),
+    }),
   ],
 };
